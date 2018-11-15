60f9e0e7d6dacdcd75307ea176e6d17c3ef6c186
Mykola Avdieiev <ronsaval@gmail.com>
2018-11-15 11:48:49 +0200

    Task started 11:48 Kyiv time

---
+++ src/test/java/GoogleSearchTest.java
@@ -0,0 +1,18 @@
+public class GoogleSearchTest {
+    /**
+     * Task:
+     * - navigate to goolge.com
+     * - enter Selenium SearchTerm
+     * - Click on Search button
+     * - Check that number of search results 10 on page 1
+     * - Check that all results contain SearchTerm
+     * - Go to Page 2
+     * - Check that search results number = 10
+     * - Check that all results contain searchTerm
+     * - Add JavaDocs for all methods
+     * - Add readme.md with detailed description
+     * - Add gitignore
+     * - Push to the GitHub
+     */
+
+}
